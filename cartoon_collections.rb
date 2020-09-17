654fc2060be8a5ea53a0ae72d960309b9bd64c77
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-17 18:20:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.any? do |string|
     string.length > 4
+  end
 end
 
 def find_valid_calls(planeteer_calls)
